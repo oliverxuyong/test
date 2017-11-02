91584c20ec683ea107d4ae7e81a820873d8f812d
oliverxuyong <oliver.xuyong@gmail.com>
2017-11-02 20:30:56 +0800

    恢复了log2root()方法,并且可以退回.

--- xunta-web/src/main/webapp/client_code/dialog_page/dialog_page_common.js
+++ xunta-web/src/main/webapp/client_code/dialog_page/dialog_page_common.js
@@ -3,7 +3,7 @@ var replySenderName = 'null'//记录点击内容回复消息时被点击用户�
 var replyOpptid = 'null';
 function afterInput(inputValue, tmpPid) {//输入框提交到inputSubmit,然后到这里(此时tmpPid="none");感叹号直接提交都到这里(此时tmpPid!=none).
 	if(inputValue == "660419"){//这句密码是为了打开index.html中的log记录.
-		openWin('root','index.html','');
+		openWin('root','index.html','showlog');
 		return;
 	}
 
