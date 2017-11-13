b4a9f6090459b79af6a8744fd69f34d5bcaddd6d
baobin.fang <614024531@qq.com>
2017-11-13 10:23:19 +0800

    将聊天页面回退功能修改

--- xunta-web/src/main/webapp/client_code/main_page/topics_page.js
+++ xunta-web/src/main/webapp/client_code/main_page/topics_page.js
@@ -180,14 +180,17 @@ function enterDialogPage(toUserId,toUserName,muImg) {
 		"userImage" : userImg,
 		"server_domain" : domain,
 		"userAgent" : userAgent,
-		"topicPageSign" : "yes"
+		"topicPageSign" : ""
 	};
 	console.log("enterDialogPage toUserId=" + toUserId + "|toUserName="
 			+ toUserName);
 	
-	//在打开聊天页的时候请求共同选择的标签
 	if(window.parent.document.getElementById(toUserId)!=null ){//聊天列表打开过
+		//在打开聊天页的时候请求共同选择的标签
 		exec(toUserId, "requestSelectCP()");
+		
+		//将_topicPageSign赋值为""
+		exec(toUserId, "setTopicPageSignIsNull()");
 	}
 	
 	// openWin(topicid,'dialog_page/dialog_page.html',JSON.stringify(pageParam));
