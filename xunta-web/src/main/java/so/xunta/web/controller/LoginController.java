f083f62be93858c0b03725eeccd382bc7695306e
baobin.fang <614024531@qq.com>
2017-12-08 09:52:29 +0800

    创建自定义菜单

--- xunta-web/src/main/java/so/xunta/web/controller/LoginController.java
+++ xunta-web/src/main/java/so/xunta/web/controller/LoginController.java
@@ -766,13 +766,13 @@ public class LoginController {
 			/**
 			 * start:2017.12.07 叶夷 删除自定义菜单
 			 */
-			logger.info("开始删除自定义菜单");
+			/*logger.info("开始删除自定义菜单");
 			String menu_delete_url = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN";
 			String deleteAccessToken=weChatService.getToken(appid, appsecret);
 			logger.info("accessToken="+deleteAccessToken);
 			String deleteUrl = menu_delete_url.replace("ACCESS_TOKEN", deleteAccessToken);
 			JSONObject deleteJsonObject=weChatService.httpRequest(deleteUrl, "GET", null);
-			logger.info("删除菜单结果:"+deleteJsonObject);
+			logger.info("删除菜单结果:"+deleteJsonObject);*/
 			/**
 			 * end:2017.12.07 叶夷  删除自定义菜单
 			 */
@@ -780,7 +780,7 @@ public class LoginController {
 			/**
 			 * start:2017.12.07 叶夷  创建自定义菜单
 			 */
-			/*logger.info("开始创建自定义菜单");
+			logger.info("开始创建自定义菜单");
 			String menu_create_url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
 			String accessToken=weChatService.getToken(appid, appsecret);
 			logger.info("accessToken="+accessToken);
@@ -799,7 +799,7 @@ public class LoginController {
 			logger.info("menuString="+menuString);
 			JSONObject jsonObject=weChatService.httpRequest(url, "POST", menuString);
 			logger.info("创建菜单结果:"+jsonObject);
-			*//**
+			/**
 			 * end:2017.12.07 叶夷  创建自定义菜单
 			 */
 			
