a75e11341503564dc3676eff31eddc8edb42f370
bright <385421253@qq.com>
2017-12-08 17:10:15 +0800

    添加EventScope和CpType，使不同EventScope的用户相互隔离3

--- xunta-websocket/src/main/java/so/xunta/websocket/utils/RecommendTaskPool.java
+++ xunta-websocket/src/main/java/so/xunta/websocket/utils/RecommendTaskPool.java
@@ -3,8 +3,6 @@ package so.xunta.websocket.utils;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,16 +16,16 @@ public class RecommendTaskPool {
 	
 	Logger logger =Logger.getLogger(RecommendTaskPool.class);
 	
+	public RecommendTaskPool() {
+		threadPoolExecutor.setRejectedExecutionHandler(wolfRejectedExecutionHandler);
+	}
+	
 	public void execute(Runnable task){	
 		try {
 			threadPoolExecutor.execute(task);
 		} catch (RejectedExecutionException e) {
 			logger.error(e.getMessage());
 		} 	
-	}
-	@PostConstruct
-	public void setRejectedHandler(){
-		threadPoolExecutor.setRejectedExecutionHandler(wolfRejectedExecutionHandler);
 	}
 	
 	public void destroy(){
