4dfa56def5f9f581880c32683a40e22709e8dc52
s23ly <s23ly@users.noreply.github.com>
2017-12-22 18:19:35 +0800

    修复推荐判断2

--- xunta-persist/src/main/java/so/xunta/persist/impl/CpChoiceDetailDaoImpl.java
+++ xunta-persist/src/main/java/so/xunta/persist/impl/CpChoiceDetailDaoImpl.java
@@ -67,11 +67,14 @@ public class CpChoiceDetailDaoImpl implements CpChoiceDetailDao {
 	@Override
 	public CpChoiceDetailDO getCpChoiceDetailBeforeTime(Long userid, BigInteger cpId,Timestamp myLastUpdateTime) {
 		Session session = sessionFactory.getCurrentSession();
-		String sql="SELECT * FROM cp_choice_detail WHERE user_id=:userId AND cp_id =:cpId AND create_time<:lastUpdateTime ORDER BY create_time DESC ";
+		String sql="SELECT * FROM cp_choice_detail WHERE user_id=:userId AND cp_id =:cpId AND create_time<:lastUpdateTime AND is_selected='Y' ORDER BY create_time DESC ";
 		@SuppressWarnings("unchecked")
 		List<CpChoiceDetailDO> result = (List<CpChoiceDetailDO>)session.createSQLQuery(sql).addEntity(CpChoiceDetailDO.class).setLong("userId", userid).setBigInteger("cpId", cpId).setTimestamp("lastUpdateTime", myLastUpdateTime).list();
-		return result.get(0);
-
+		if(result.size()!= 0){
+			return result.get(0);
+		}else{
+			return null;
+		}
 	}
 
 }
