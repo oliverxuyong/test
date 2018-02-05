952e8f523ee223e2feb8e17d7beda9efaaab6be9
baobin.fang <614024531@qq.com>
2018-02-05 16:55:45 +0800

    js代码动态加载js和css文件

--- xunta-web/src/main/webapp/client_code/matchUsers_page/dialoglist_page_common.js
+++ xunta-web/src/main/webapp/client_code/matchUsers_page/dialoglist_page_common.js
@@ -1,6 +1,6 @@
 function requestDialogList(){
 	$.ajax({
-        url:"http://xunta.so:3000/v1/chat_list",
+        url:"https://xunta.so:3000/v1/chat_list",
         type:"POST",
         dataType:"jsonp",
         jsonp:"callback",
