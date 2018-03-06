aaefb317aee84db798b0b6d3c7d5fa95792cfb67
baobin.fang <614024531@qq.com>
2018-03-06 12:46:17 +0800

    微信小程序登录测试

--- xunta-web/src/main/java/so/xunta/web/controller/WXAppletUnionidController.java
+++ xunta-web/src/main/java/so/xunta/web/controller/WXAppletUnionidController.java
@@ -1,13 +1,9 @@
 package so.xunta.web.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Writer;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.GetMethod;
