9fabd0aed3db294395765123e2c838c46c4ec78b
baobin.fang <614024531@qq.com>
2018-03-15 13:26:06 +0800

    1.将公司简介等按钮放入了main_page页面   2.popup.js中第一个方法是用来封装api的方法，不要注释

--- xunta-web/src/main/webapp/client_code/main_page/topics_page.js
+++ xunta-web/src/main/webapp/client_code/main_page/topics_page.js
@@ -366,3 +366,14 @@ function sendGuidePageAddTag(userId){
 	var paraStr = userId;
 	execRoot("sendGuidePageAddTag('"+ paraStr +"')");
 }
+
+//2018.02.05  叶夷     进入公司简介页面
+function enterProfilePage() {
+	console.log("enterProfilePage");
+	openWin('profile_page', preUrl+'/xunta-web/client_code/profile_page/profile_page.html', "");
+}
+//2018.02.05  叶夷     进入产品介绍页面
+function enterProductPage() {
+	console.log("enterProductPage");
+	openWin('product_page', preUrl+'/xunta-web/client_code/product_page/product_page.html', "");
+}
