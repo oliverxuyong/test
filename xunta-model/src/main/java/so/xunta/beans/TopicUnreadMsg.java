d76cb69a3481ade699f6020fabd14f5ddd56bf4b
baobin.fang <614024531@qq.com>
2018-04-09 17:39:34 +0800

    TopicUnreadMsg类一直找不到的原因是@Id的包导错

--- xunta-model/src/main/java/so/xunta/beans/TopicUnreadMsg.java
+++ xunta-model/src/main/java/so/xunta/beans/TopicUnreadMsg.java
@@ -4,8 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
-import org.springframework.data.annotation.Id;
 
 /**
  * 2018.03.30
