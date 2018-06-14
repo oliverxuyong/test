4a8880b6cad5c8df5c04252d9df67a92820a2ece
baobin.fang <614024531@qq.com>
2018-06-14 13:58:06 +0800

    测试获取客户端的IP地址

--- xunta-web/src/main/java/so/xunta/web/controller/UserController.java
+++ xunta-web/src/main/java/so/xunta/web/controller/UserController.java
@@ -38,6 +38,10 @@ public class UserController {
 	@RequestMapping("/checkuser")
 	public void checkUserExist(String userid,String userimage,HttpServletRequest request,HttpServletResponse response) throws IOException{
 		logger.info("IP地址："+request.getAttribute("X-Forwarded-For"));
+		
+        logger.info("ipFromNginx:" + request.getAttribute("X-Real-IP"));  
+        logger.info("getRemoteAddr:" + request.getRemoteAddr());  
+		
 		logger.debug("checkuser userid:"+userid);
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/json");
